7a8a7e1421984c631931956b57148cecb78049a4
carlosSalda <carlosjoelsalda@gmail.com>
2021-03-13 03:52:16 -0300

    primeros avances movieSearch

---
+++ movieSearch-React-redux/src/pages/Home/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import {
+  Container,
+  Grid,
+  Typography,
+  Card,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import styles from "./style";
+
+import { MovieIcon } from "../../icons/index";
+
+export default ({history}) => {
+  const [searchText, setSearchText] = useState("");
+  const classes = styles();
+
+
+  const handleSearchTextChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
+  const handleCleanText = (event) => {
+		setSearchText('');
+  };
+
+  const handleSearchText = (event) => {
+		history.push(`/results?MovieName=${searchText}`);
+  };
+
+  return (
+    <Container className={classes.container}>
+      <Card className={classes.cardContainer}>
+        <Grid container className={classes.titleGridContainer}>
+          <Grid>
+            <Typography className={classes.title}> Bienvenido!</Typography>
+          </Grid>
+          <Grid>
+            <MovieIcon className={classes.movieIcon} />
+          </Grid>
+        </Grid>
+        <TextField
+          value={searchText}
+          placeholder="Buscar..."
+          onChange={handleSearchTextChange}
+          className={classes.textFieldSearch}
+        ></TextField>
+        <Grid className={classes.buttonsContainer}>
+          <Button variant="contained" onClick={handleCleanText}>
+            Limpiar
+          </Button>
+
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            className={classes.searchButton}
+            onClick={handleSearchText}
+          >
+            Buscar
+          </Button>
+        </Grid>
+      </Card>
+    </Container>
+  );
+};
---
+++ movieSearch-React-redux/src/pages/Results/index.js
@@ -0,0 +1,23 @@
+import { Container } from '@material-ui/core';
+import React, {useEffect} from 'react';
+import queryString from 'query-string';
+
+import { useDispatch } from 'react-redux';
+import {searchMovie} from '../../redux/actions/search';
+
+
+export default ({location}) => {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const {movieName} = queryString.parse(location.search);
+        dispatch(searchMovie({movieName}));
+    })
+
+    return(
+        <Container>
+                Results
+        </Container>
+    )
+}
